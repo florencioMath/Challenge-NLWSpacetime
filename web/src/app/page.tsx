ff71a269f63bbd69b96aa1b6fa5fe9d8f1b27485
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-05-16 21:50:40 -0300

    feat: Font config.

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1>florencioMath</h1>
+      <h1 className="font-sans">florencioMath</h1>
+      <h1 className="font-alt">florencioMath</h1>
     </>
   );
 }
