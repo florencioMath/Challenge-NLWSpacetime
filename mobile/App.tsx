d470cab756d3a2ddaa0fffc88c3c5eee62d33b9a
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-05-17 15:39:59 -0300

    feat: Font configuration.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-zinc-800">
-      <Text className="text-5xl text-zinc-50">florencioMath</Text>
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="font-body text-5xl text-zinc-50">florencioMath</Text>
       <StatusBar style="light" />
     </View>
   );
