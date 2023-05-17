3d83cb93caba5c208ca956bb00b48d5d29e9615d
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-05-17 20:58:48 -0300

    feat: Authorization route.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
+import 'dotenv/config';
+
 import fastify from 'fastify';
 import cors from '@fastify/cors';
 import { memoriesRoute } from './routes/memories';
+import { authRoutes } from './routes/auth';
 
 const app = fastify();
 
@@ -9,6 +12,7 @@ app.register(cors, {
 });
 
 app.register(memoriesRoute);
+app.register(authRoutes);
 
 app
   .listen({
