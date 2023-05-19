4abfe846426ce74a5e6dfda49afda16fee990e2f
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-05-19 20:00:48 -0300

    Web-feat: Showing memories.

--- web/src/components/EmptyMemories.tsx
+++ web/src/components/EmptyMemories.tsx
@@ -1,4 +1,4 @@
-export function EmpyyMemories() {
+export function EmptyMemories() {
   return (
     <div className='flex flex-1 items-center justify-center'>
       <p className='w-[360px] text-center leading-relaxed'>
